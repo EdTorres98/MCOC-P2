d6aa28bf293766f67034268b0aef828e93ce7780
EdTorres98 <69275311+EdTorres98@users.noreply.github.com>
2020-09-28 18:05:56 -0300

    Implementando funciones de Barra

--- barra.py
+++ barra.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 27 17:58:58 2020
-
-@author: Felipe Bravo
-"""
-
 import numpy as np
 
 class Barra(object):
@@ -17,20 +10,27 @@ class Barra(object):
         self.E = E
         self.ρ = ρ
         self.σy = σy
+
     def obtener_conectividad(self):
-        self.c = [self.ni,self.nj]        
-        return self.c
+        return [self.ni,self.nj]
+
     def calcular_area(self):
-        self.A = np.pi()*(self.R**2-(self.R-self.t)**2)
+        A = np.pi*(self.R**2-(self.R-self.t)**2)
         return A
+
     def calcular_largo(self,reticulado):
         ret = reticulado
         xi = ret.obtener_coordenada_nodal(self.ni)
-        xj = ret.obtener_coordenada_nodal(self.ni)
-        L = xi-xj
-        return np.sqrt(L(0)**2+L(1)**2+L(2)**2)
-    def calcular_peso(self):
-        self.p =  self.ρ*self.A*self.L
-        return self.p
-    
-    #probando
+        xj = ret.obtener_coordenada_nodal(self.nj)
+        dij = xi-xj
+        return np.sqrt(np.dot(dij,dij))
+
+    def calcular_peso(self,reticulado):
+        A = np.pi*(self.R)**2-np.pi*(self.R-self.t)**2
+        ret = reticulado
+        xi = ret.obtener_coordenada_nodal(self.ni)
+        xj = ret.obtener_coordenada_nodal(self.nj)
+        dij = xi-xj
+        L=np.sqrt(np.dot(dij,dij))
+        p = self.ρ * A*L
+        return p
